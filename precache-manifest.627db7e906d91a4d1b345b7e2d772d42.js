fb3607d3d4ab7c75e33b06b6c3b34b653dac79df
maxnitude <mailbox.timofeev@gmail.com>
2021-02-27 18:29:06 +0300

    Updates

---
+++ precache-manifest.627db7e906d91a4d1b345b7e2d772d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49297937829a06c3816797533d1d6ba",
+    "url": "/space/index.html"
+  },
+  {
+    "revision": "31188a919d125b760ad2",
+    "url": "/space/static/css/main.628c67ed.chunk.css"
+  },
+  {
+    "revision": "305c278d22516402b004",
+    "url": "/space/static/js/2.8f907318.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/space/static/js/2.8f907318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31188a919d125b760ad2",
+    "url": "/space/static/js/main.1c355a2d.chunk.js"
+  },
+  {
+    "revision": "229f3dcebe12c94c9971",
+    "url": "/space/static/js/runtime-main.dcb01884.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/space/static/media/logo.d5ab31ce.svg"
+  }
+]);
